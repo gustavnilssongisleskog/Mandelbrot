000d2c054ef20aff3d57c627c8a7fb2626b7c074
gustavnilssongisleskog <gisleskoggustav@gmail.com>
2021-06-22 01:46:15 +0200

    added border tracing, and some experiments with threads

--- src/main/scala/fractal/mandelbrot/Mandelbrot.scala
+++ src/main/scala/fractal/mandelbrot/Mandelbrot.scala
@@ -1,5 +1,6 @@
 package fractal.mandelbrot
 import fractal.Complex
+import scala.collection.mutable.{Queue, Set}
 
 case class Mandelbrot(reDim: Int = 700, //reDim: Int = 1000, imDim: Int = 1000,
                      reMid: Double = -0.5, reSize: Double = 2,
@@ -25,16 +26,85 @@ case class Mandelbrot(reDim: Int = 700, //reDim: Int = 1000, imDim: Int = 1000,
     private val repsArray = Array.fill(reDim)(Array.fill(imDim)(-1))
 
     private def evaluate(): Unit = {
-        def evaluate(x0: Int, y0: Int)(x1: Int, y1: Int): Unit = {
 
+        /*def evaluate(x: Int, y: Int): Unit = {
+            repsArray(x)(y) = repetitions(pixelToComplex(x, imDim - 1 - y))
+        }
+      
+        def evaluateLine(y: Int): Unit = {
+            (0 until reDim).foreach(x => evaluate(x, y))
+        }
+
+        val t0 = System.currentTimeMillis
+        val available = 2 * Runtime.getRuntime.availableProcessors
+        println(available)
+        val threads = Vector.tabulate(available)(y => new Thread(){
+            override def run(): Unit = {
+                val t0 = System.currentTimeMillis
+                Range(y, imDim, available).foreach(evaluateLine)
+                val t1 = System.currentTimeMillis
+                println(s"Thread #$y: ${t1 - t0}")
+            }
+        })
+
+        //val t1 = System.currentTimeMillis
+
+        threads.foreach(_.start)
+        threads.foreach(_.join)
+
+        //val t2 = System.currentTimeMillis
+        //println(s"Initialize: ${t1 - t0}")
+        //println(s"Execute:    ${t2 - t1}")
+        //println(s"Total:      ${t2 - t0}")
+*/
+
+        def evaluate(x: Int, y: Int): Int = {
+            if repsArray(x)(y) == -1 then 
+                repsArray(x)(y) = repetitions(pixelToComplex(x, imDim - 1 - y))
+            repsArray(x)(y)
+        }
+        
+        val qIn = Queue[(Int, Int)]()
+        val qOut = Queue[(Int, Int)]()
+        val visited = Set[(Int, Int)]()
+
+        def isOutside(x: Int, y: Int): Boolean = x < 0 || x >= reDim || y < 0 || y >= imDim
+
+        def neighbors(p: (Int, Int)) = Vector((p._1 - 1, p._2 - 1), (p._1, p._2 - 1), (p._1 + 1, p._2 - 1), (p._1 - 1, p._2), (p._1 + 1, p._2), (p._1 - 1, p._2 + 1), (p._1, p._2 + 1), (p._1 + 1, p._2 + 1))
+        
+        def borderTrace(x: Int, y: Int): Unit = {
+            qOut.enqueue((x, y))
+            visited.add((x, y))
+
+            while(qOut.nonEmpty) do
+                neighbors(qOut.front).filterNot(visited).foreach(investigate)
+                qOut.dequeue
+        }
+
+        def investigate(p: (Int, Int)): Unit = {
+            val xs = neighbors(p).filter((x, y) => !isOutside(x, y) && evaluate(x, y) == maxReps)
+            if xs.nonEmpty && (isOutside(p._1, p._2) || repsArray(p._1)(p._2) != maxReps) then 
+                qOut.enqueue(p)
+                visited.add(p)
+            qIn.addAll(xs)
+        }
+
+        def fill(): Unit = {
+            while(qIn.nonEmpty) do
+                neighbors(qIn.front).filter((x0, y0) => !isOutside(x0, y0) && repsArray(x0)(y0) == -1).foreach((x0, y0) =>
+                    repsArray(x0)(y0) = maxReps
+                    qIn.addOne(x0, y0)
+                )
+                qIn.dequeue
         }
 
         for
             x <- 0 until reDim
             y <- 0 until imDim
         do
-            repsArray(x)(y) = repetitions(pixelToComplex(x, imDim - 1 - y))
-
+            if repsArray(x)(y) == -1 && evaluate(x, y) == maxReps then
+                    borderTrace(x - 1, y)
+                    fill()
     }
 
     def printGrid(): Unit = {
